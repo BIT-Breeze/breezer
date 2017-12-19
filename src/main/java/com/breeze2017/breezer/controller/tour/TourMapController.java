d979dccf6a1d92d73c6eb9a773dede72a76768df
twin10240 <twin10240@naver.com>
2017-12-19 17:02:26 +0900

    Commit test.

--- src/main/java/com/breeze2017/breezer/controller/tour/TourMapController.java
+++ src/main/java/com/breeze2017/breezer/controller/tour/TourMapController.java
@@ -1,5 +1,20 @@
 package com.breeze2017.breezer.controller.tour;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.breeze2017.breezer.service.tour.TourMapService;
+
+@Controller
 public class TourMapController {
+	
+	@Autowired
+	private TourMapService tourMapService;
+	
+	@RequestMapping("/map")
+	public String mapTest() {
+		return "tour/tour_map";
+	}
 
 }
--- src/main/java/com/breeze2017/breezer/service/tour/TourMapService.java
+++ src/main/java/com/breeze2017/breezer/service/tour/TourMapService.java
@@ -1,5 +1,8 @@
 package com.breeze2017.breezer.service.tour;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TourMapService {
 
 }
