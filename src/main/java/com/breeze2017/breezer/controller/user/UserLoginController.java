1e91ea6db9115a5758abd85dac6bab5b028d695e
PureAppCrystal <purecrystar@gmail.com>
2017-12-19 14:22:17 +0900

    Update UserLoginController
    add log

--- src/main/java/com/breeze2017/breezer/controller/user/UserLoginController.java
+++ src/main/java/com/breeze2017/breezer/controller/user/UserLoginController.java
@@ -15,39 +15,41 @@ public class UserLoginController {
 
 	@Autowired
 	private HttpServletRequest request;
-	
+
 	// 로그인 화면
 	@RequestMapping({ "/", "/login" })
-	public String index( Model model ) {
+	public String index(Model model) {
+		System.out.println("====== UserLoginController : {'/', '/login} ======");
+		
 
 		// 세션의 authUser 가져오기
 		HttpSession session = request.getSession();
-		UserVo vo = (UserVo) session.getAttribute("authUser");
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
 
 		// 세션이 없다면
 		// if (authUser.getIdx() == 0) {
-		if (vo == null) {
+		if (authUser == null) {
 			System.out.println("/ : authUser is null");
 			return "user/user_login";
 		}
-		// // 세션이 있지만 id가 없다면
-		// else if (authUser.getId() == null) {
-		// return "user/join";
-		// }
+		// 세션이 있지만 id가 없다면
+		else if (authUser.getId() == null) {
+			return "user/join";
+		}
 		// 세션이 있다면
 		else {
 			System.out.println("/ : authUser is exist");
-			System.out.println("authUser : " + vo);
+			System.out.println("authUser : " + authUser);
 
-			model.addAttribute("id", vo.getId());
+			model.addAttribute("id", authUser.getId());
 			return "user/user_main";
-			
-//			RedirectView redirectView = new RedirectView("/"+vo.getId());
-//			redirectView.setContextRelative(true);
-//			
-//			ModelAndView mav = new ModelAndView(redirectView);
-//			return mav;
-			
+
+			// RedirectView redirectView = new RedirectView("/"+vo.getId());
+			// redirectView.setContextRelative(true);
+			//
+			// ModelAndView mav = new ModelAndView(redirectView);
+			// return mav;
+
 		}
 	}
 
