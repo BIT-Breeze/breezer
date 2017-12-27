15ac0300ac84e2b38bb83a996a45df94a15db981
bit-user <gusdn6526@gmail.com>
2017-12-27 10:20:53 +0900

    Modify PostAddController

--- src/main/java/com/breeze2017/breezer/controller/post/PostAddController.java
+++ src/main/java/com/breeze2017/breezer/controller/post/PostAddController.java
@@ -1,5 +1,32 @@
 package com.breeze2017.breezer.controller.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.breeze2017.breezer.service.post.PostAddService;
+import com.breeze2017.breezer.vo.PostVo;
+import com.breeze2017.security.Auth;
+
+@Controller
+@RequestMapping("/{id}")
 public class PostAddController {
 
+	@Autowired
+	PostAddService postAddService;
+	
+	@Auth
+	@RequestMapping(value="post/add", method=RequestMethod.POST)
+	public String addPost(@ModelAttribute PostVo vo) {
+		System.out.println(" >> PostAddController post/add");
+		System.out.println(vo);
+		postAddService.insertMessage(vo);
+		System.out.println("POST DATA 디비에 입력까지 완료!!");
+		
+		return "";
+	}
+	
+	
 }
