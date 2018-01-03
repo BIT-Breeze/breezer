fae9faa39c0fd826e7345e06cd7fab841df5f20c
HailFederer <urimaumdero@naver.com>
2018-01-03 13:28:26 +0900

    Add codes for getting posts list of one tour

--- src/main/java/com/breeze2017/breezer/controller/tour/TourMainController.java
+++ src/main/java/com/breeze2017/breezer/controller/tour/TourMainController.java
@@ -1,16 +1,40 @@
 package com.breeze2017.breezer.controller.tour;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.breeze2017.breezer.service.tour.TourMainService;
+import com.breeze2017.breezer.vo.PostVo;
+import com.breeze2017.breezer.vo.UserVo;
 
 @Controller
 @RequestMapping("/{id}/tour")
 public class TourMainController {
+	
+	private TourMainService tourMainService;
 
 	@RequestMapping("")
-	public String mytour(HttpServletRequest request) {
+	public String mytour(
+			HttpSession session,
+			@RequestParam(value="idx", required=false) int tourIdx,
+			Model model) {
+		
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		String userId = authUser.getId();
+		
+		List<PostVo> tour = tourMainService.getTour(userId, tourIdx);
+		
+		model.addAttribute("tour", tour);
+		model.addAttribute("tourIdx", tourIdx);
 		
 		return "tour/tour_main";
 	}
--- src/main/java/com/breeze2017/breezer/repository/tour/TourMainDao.java
+++ src/main/java/com/breeze2017/breezer/repository/tour/TourMainDao.java
@@ -1,16 +1,29 @@
 package com.breeze2017.breezer.repository.tour;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.breeze2017.breezer.vo.TourVo;
+import com.breeze2017.breezer.vo.PostVo;
 
 @Repository
 public class TourMainDao {
 	
-
-
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<PostVo> getTour(String userId, int tourIdx){
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("userId", userId);
+		map.put("tourIdx", tourIdx);
+		
+		List<PostVo> tour = sqlSession.selectList("tourMain.getTour", map);
+		
+		return tour;
+	}
 }
